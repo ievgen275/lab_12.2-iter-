71ce3be336659e99f509efe1a7c35d0d6c8bcc5d
ievgen275 <ievgen3435@gmail.com>
2024-05-01 21:26:00 +0300

    first commit

---
+++ PR_12.2(iter)/PR_12.2(iter).cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+typedef int Info;
+struct Elem {
+    Elem* link;
+    Info info;
+};
+
+Elem* p = nullptr;
+
+void append(Info newData) {
+    Elem* newNode = new Elem();
+    newNode->info = newData;
+    newNode->link = nullptr;
+
+    if (p == nullptr) {
+        p = newNode;
+        return;
+    }
+
+    Elem* lastNode = p;
+    while (lastNode->link != nullptr) {
+        lastNode = lastNode->link;
+    }
+    lastNode->link = newNode;
+}
+
+void increaseListValues(Info increaseBy) {
+    Elem* current = p;
+    while (current != nullptr) {
+        current->info += increaseBy;
+        current = current->link;
+    }
+}
+
+void printList() {
+    Elem* node = p;
+    while (node != nullptr) {
+        cout << node->info << " ";
+        node = node->link;
+    }
+    cout << endl;
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    append(1);
+    append(2);
+    append(3);
+    append(4);
+    append(5);
+
+    cout << "Початковий список: ";
+    printList();
+
+    Info increaseBy;
+    cout << "Введіть на скільки збільшити значення кожного елемента: ";
+    cin >> increaseBy;
+
+    increaseListValues(increaseBy);
+
+    cout << "Змінений список: ";
+    printList();
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR_12.2(iter)/PR_12.2(iter).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+        TEST_METHOD(TestAppend)
+        {
+            Info testData = 10;
+
+            append(testData);
+
+            Assert::AreEqual(testData, p->info);
+        }
+	};
+}
